aa4207f580e38c4fd8d21a86496c52571fda8453
Bruno Darshan <brunodarshan@gmail.com>
2019-09-30 11:36:17 -0300

    reinclude coins resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :coins, only: [:create]
   resources :users, except: [:index, :show, :create, :new, :edit, :destroy] do
     delete 'monster/:monster_id', to: 'monsters#destroy'
   end
