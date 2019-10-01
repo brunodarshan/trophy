8d459f83981b7abc349a6f215f7dab037f7ac417
Bruno Darshan <brunodarshan@gmail.com>
2019-10-01 16:28:18 -0300

    add killed monster job to controller

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -1,21 +1,19 @@
 # frozen_string_literal: true
 
 class MonstersController < ApplicationController
-  
-
   def destroy
     @killed_monster = KilledMonster.new(killed_monster_params)
     if @killed_monster.save
       @user = User.find(killed_monster_params[:user_id])
       @monster = Monster.find(killed_monster_params[:monster_id])
+      Audit::KilledMonsterWorker.perform_async(@user.id, @monster.id)
       render json: {
         msg: :success, data: "#{@user.username} killed #{@monster.name}"
       }
-        # TO DO process data and verify if has trophy
     else
-        render json: {
-            msg: :error, data: @killed_monster.errors
-        }, status: :unauthorized
+      render json: {
+        msg: :error, data: @killed_monster.errors
+      }, status: :unauthorized
     end
   end
 
--- spec/controllers/monsters_controller_spec.rb
+++ spec/controllers/monsters_controller_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'sidekiq/testing'
+Sidekiq::Testing.fake!
 
 RSpec.describe MonstersController, type: :controller do
   context 'DESTROY /users/:user_id/monster/:monster_id/' do
@@ -10,8 +12,16 @@ RSpec.describe MonstersController, type: :controller do
       delete :destroy, params: { user_id: user_id, monster_id: monster_id }
     end
 
+    after do 
+      Sidekiq::Worker.clear_all
+    end
+
     it 'respond with status success' do
       expect(response.status).to eql(200)
+    end
+
+    it 'enqueue a killed monster job in background' do
+      expect(Audit::KilledMonsterWorker.jobs.size).to eql(1)
     end
 
     it %(data is 'killer1000 killed Baby Shark') do
